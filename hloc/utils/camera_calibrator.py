bd4d8559ce34b6d49e88d1cea27e204f599428bb
Michal Polic <michal.polic@gmail.com>
2024-02-09 21:48:45 +0000

    Finish localization pipelines

--- hloc/utils/camera_calibrator.py
+++ hloc/utils/camera_calibrator.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import glob
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-def calibrate_camera(image_dir, rows, cols, square_edge_length):
+def calibrate_camera(image_dir, rows, cols, square_edge_length, plot_corners=True):
     """
     Calibrate a camera using chessboard images.
 
@@ -11,48 +12,55 @@ def calibrate_camera(image_dir, rows, cols, square_edge_length):
     - image_dir: Path to the directory containing chessboard images.
     - rows, cols: Number of corners along the chessboard's rows and columns.
     - square_edge_length: Length of the edge of a chessboard square in millimeters.
+    - plot_corners: If True, plot images with detected corners.
 
     Returns:
     - focal_length: The focal length of the camera.
     - principal_point: The principal point of the camera.
-    - radial_distortion: One coefficient for the polynomial radial distortion model.
+    - distortion_coefficients: Distortion coefficients of the camera.
+    - image_size: The size of the calibration images (width, height).
     """
 
-    # Termination criteria for corner sub-pixel accuracy
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # Prepare object points
     objp = np.zeros((rows*cols, 3), np.float32)
     objp[:,:2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2) * square_edge_length
 
-    object_points = []  # 3D points in real world space
-    image_points = []  # 2D points in image plane
+    object_points = []
+    image_points = []
+
+    # Create directory for images with detected corners
+    corners_dir = Path(image_dir.parent) / (f"{image_dir.name}_corners")
+    corners_dir.mkdir(exist_ok=True)
 
     images = glob.glob(str(Path(image_dir) / '*.jpg'))
 
     for fname in images:
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        height, width = gray.shape
+        width, height = gray.shape
 
-        # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
 
-        # If found, add object points, image points
         if ret:
             object_points.append(objp)
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             image_points.append(corners2)
 
-    # Camera calibration
+            if plot_corners:
+                cv2.drawChessboardCorners(img, (rows, cols), corners2, ret)
+                 # Generate path for the new image with corners
+                corner_img_path = corners_dir / Path(fname).name
+                cv2.imwrite(str(corner_img_path), img)
+
+
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
-    print("Error in projection : \n", ret) 
-    print("\nCamera matrix : \n", mtx) 
-    print("\nDistortion coefficients : \n", dist) 
+
+    print("Reprojection error: \n", ret) 
+    print("\nCamera matrix: \n", mtx) 
+    print("\nDistortion coefficients: \n", dist) 
 
     focal_length = (mtx[0, 0], mtx[1, 1])  # fx, fy
     principal_point = (mtx[0, 2], mtx[1, 2])  # cx, cy
-    #radial_distortion = dist[0][0]   Assuming one coefficient for radial distortion
 
     return focal_length, principal_point, dist, (width, height)
 
